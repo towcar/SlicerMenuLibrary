f757b3a7b691e33a5823a465610a5a151344610c
towcar <Carson.skjerdal@Gmail.com>
2018-04-28 14:16:30 -0700

    Fixed Sample Demo to improve example accessibility

--- app/src/main/java/com/carsonskjerdal/slicermenusample/MainActivity.java
+++ app/src/main/java/com/carsonskjerdal/slicermenusample/MainActivity.java
@@ -1,18 +1,10 @@
 package com.carsonskjerdal.slicermenusample;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.carsonskjerdal.slicermenu.animation.SlicerAnimation;
