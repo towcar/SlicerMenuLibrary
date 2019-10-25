ec737b116bb1fc7bf3450fc60e9ebc3911409bf3
towcar <Carson.skjerdal@Gmail.com>
2019-10-25 08:12:09 -0700

    Updated Compatibility

--- app/src/main/java/com/carsonskjerdal/slicermenusample/ButtonClickActivity.java
+++ app/src/main/java/com/carsonskjerdal/slicermenusample/ButtonClickActivity.java
@@ -1,8 +1,8 @@
 package com.carsonskjerdal.slicermenusample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +13,6 @@ import com.carsonskjerdal.slicermenu.animation.SlicerAnimation;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Created by Carson on 4/29/2018.
--- app/src/main/java/com/carsonskjerdal/slicermenusample/SimpleActivity.java
+++ app/src/main/java/com/carsonskjerdal/slicermenusample/SimpleActivity.java
@@ -1,8 +1,8 @@
 package com.carsonskjerdal.slicermenusample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
